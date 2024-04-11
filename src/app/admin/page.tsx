ea6f1eb22b90a19aee888973684ff7a75a7b5dc2
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-04-11 11:31:10 +0200

    Load data to dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,18 +5,34 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { getSalesData } from "@/data/admin";
+import { getProductData, getSalesData, getUserData } from "@/data/admin";
 import { DashboardCardProps } from "@/lib/definitions";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
 
 export default async function AdminDashBoard() {
-  const salesData = await getSalesData();
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       <DashboardCard
         title='Sales'
         subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title='Customer'
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title='Active Products'
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
