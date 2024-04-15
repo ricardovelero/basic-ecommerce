84212a88a9b63fa0c7b676e033321c09c083b516
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-04-15 11:42:57 +0200

    Add success page

--- src/app/(public)/products/[id]/purchase/_components/checkout-form.tsx
+++ src/app/(public)/products/[id]/purchase/_components/checkout-form.tsx
@@ -37,32 +37,30 @@ export default function CheckoutForm({
   clientSecret,
 }: CheckoutFormProps) {
   return (
-    <>
-      <div className='max-w-5xl w-full mx-auto space-y-8'>
-        <div className='flex items-center gap-4'>
-          <div className='aspect-video flex-shrink-0 w-1/3 relative'>
-            <Image
-              src={product.imagePath}
-              alt={product.name}
-              fill
-              className='object-cover'
-            />
+    <div className='max-w-5xl w-full mx-auto space-y-8'>
+      <div className='flex items-center gap-4'>
+        <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div>
+          <div className='text-lg'>
+            {formatCurrency(product.priceInCents / 100)}
           </div>
-          <div>
-            <div className='text-lg'>
-              {formatCurrency(product.priceInCents / 100)}
-            </div>
-            <h1 className='text-2xl font-bold'>{product.name}</h1>
-            <div className='line-clamp-3 text-muted-foreground'>
-              {product.description}
-            </div>
+          <h1 className='text-2xl font-bold'>{product.name}</h1>
+          <div className='line-clamp-3 text-muted-foreground'>
+            {product.description}
           </div>
         </div>
-        <Elements stripe={stripePromise} options={{ clientSecret }}>
-          <Form priceInCents={product.priceInCents} productId={product.id} />
-        </Elements>
       </div>
-    </>
+      <Elements stripe={stripePromise} options={{ clientSecret }}>
+        <Form priceInCents={product.priceInCents} productId={product.id} />
+      </Elements>
+    </div>
   );
 }
 
--- src/app/(public)/stripe/purchase-success/page.tsx
+++ src/app/(public)/stripe/purchase-success/page.tsx
@@ -1,3 +1,65 @@
-export default function SuccessPage() {
-  return <div>SuccessPage</div>;
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { formatCurrency } from "@/lib/formatters";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function SuccessPage({
+  searchParams,
+}: {
+  searchParams: { payment_intent: string };
+}) {
+  const paymentIntent = await stripe.paymentIntents.retrieve(
+    searchParams.payment_intent
+  );
+
+  if (paymentIntent.metadata.productId == null) return notFound();
+
+  const product = await db.product.findUnique({
+    where: {
+      id: paymentIntent.metadata.productId,
+    },
+  });
+
+  if (product == null) return notFound();
+
+  const isSuccess = paymentIntent.status === "succeeded";
+
+  return (
+    <div className='max-w-5xl w-full mx-auto space-y-8'>
+      <h1 className='text-4xl font-bold'>
+        {isSuccess ? "Success!" : "Something went wrong"}
+      </h1>
+      <div className='flex items-center gap-4'>
+        <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div>
+          <div className='text-lg'>
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className='text-2xl font-bold'>{product.name}</h1>
+          <div className='line-clamp-3 text-muted-foreground'>
+            {product.description}
+          </div>
+          <Button className='mt-4' size='lg' asChild>
+            {isSuccess ? (
+              <a href=''></a>
+            ) : (
+              <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
+            )}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
